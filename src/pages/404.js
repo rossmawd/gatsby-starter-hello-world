7a684b3ec3e033ad580c62a250f9ea96c97091a8
Ross Mawdsley <rossmawd@gmail.com>
2020-10-03 17:42:28 +0100

    added custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styles from './404.module.scss'
 
 export default () => {
   return (
-    <div>
-      Not found!
+    <div className={styles.content}>
+      <h1 className={styles.header} >Page not found</h1>
+      <p className={styles.errorMessage}>the page you are looking for does not exist</p>
       {/* <a href="/about">About</a> | <a href="/">Home</a>  */}
       <Link to="/about">About</Link>  <Link to="/">Home</Link>
 
